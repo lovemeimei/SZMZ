c8ded0a491b868edc4ee9c5aabeec0b5f5138ca5
qieyixuan <386638790@qq.com>
2017-09-08 14:51:08 +0800

    扫码

---
+++ app/src/main/java/com/barcode/CaptureActivity.java
@@ -0,0 +1,260 @@
+package com.barcode;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.res.AssetFileDescriptor;
+import android.graphics.Bitmap;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.media.MediaPlayer.OnCompletionListener;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.os.Vibrator;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceHolder.Callback;
+import android.view.SurfaceView;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.barcode.camera.CameraManager;
+import com.barcode.decoding.CaptureActivityHandler;
+import com.barcode.decoding.DecodeFormatManager;
+import com.barcode.decoding.InactivityTimer;
+import com.barcode.decoding.Intents;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.Result;
+import com.szmz.R;
+
+import java.io.IOException;
+import java.util.Vector;
+
+public class CaptureActivity extends AppCompatActivity implements Callback{
+
+	private CaptureActivityHandler handler;
+	private ViewfinderView viewfinderView;
+	private boolean hasSurface;
+	private Vector<BarcodeFormat> decodeFormats;
+	private String characterSet;
+	private TextView resultView;
+	private InactivityTimer inactivityTimer;
+	private MediaPlayer mediaPlayer;
+	private boolean playBeep;
+	private static final float BEEP_VOLUME = 0.10f;
+	private boolean vibrate;
+	private boolean isOpenFlash = false;
+	Toolbar toolbar;
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.capture);
+		// 初始化 CameraManager
+		CameraManager.init(getApplication());
+
+		toolbar =(Toolbar) findViewById(R.id.toolbar);
+		setSupportActionBar(toolbar);
+		ActionBar actionBar = getSupportActionBar();
+		if (actionBar != null) {
+			actionBar.setDisplayHomeAsUpEnabled(true);
+			toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+				@Override
+				public void onClick(View v) {
+					onBackPressed();
+				}
+			});
+		}
+
+
+		viewfinderView = (ViewfinderView) findViewById(R.id.viewfinder_view);
+		resultView = (TextView) findViewById(R.id.result_view);
+		resultView.setVisibility(View.GONE);
+		hasSurface = false;
+		inactivityTimer = new InactivityTimer(this);
+		characterSet = getIntent().getStringExtra(Intents.Scan.CHARACTER_SET);
+		decodeFormats = DecodeFormatManager.parseDecodeFormats(getIntent());
+
+	}
+
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		SurfaceView surfaceView = (SurfaceView) findViewById(R.id.preview_view);
+		SurfaceHolder surfaceHolder = surfaceView.getHolder();
+		if (hasSurface) {
+			initCamera(surfaceHolder);
+		} else {
+			surfaceHolder.addCallback(this);
+			surfaceHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+		}
+
+		playBeep = true;
+		AudioManager audioService = (AudioManager) getSystemService(AUDIO_SERVICE);
+		if (audioService.getRingerMode() != AudioManager.RINGER_MODE_NORMAL) {
+			playBeep = false;
+		}
+		initBeepSound();
+		vibrate = true;
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+		if (handler != null) {
+			handler.quitSynchronously();
+			handler = null;
+		}
+		CameraManager.get().closeDriver();
+	}
+
+	@Override
+	protected void onDestroy() {
+		inactivityTimer.shutdown();
+		super.onDestroy();
+	}
+
+	private void initCamera(SurfaceHolder surfaceHolder) {
+		try {
+			CameraManager.get().openDriver(surfaceHolder);
+		} catch (IOException ioe) {
+			return;
+		} catch (RuntimeException e) {
+			return;
+		}
+		if (handler == null) {
+			handler = new CaptureActivityHandler(this, decodeFormats,
+			        characterSet);
+		}
+	}
+
+	@Override
+	public void surfaceChanged(SurfaceHolder holder, int format, int width,
+	        int height) {
+
+	}
+
+	@Override
+	public void surfaceCreated(SurfaceHolder holder) {
+		if (!hasSurface) {
+			hasSurface = true;
+			initCamera(holder);
+		}
+
+	}
+
+	@Override
+	public void surfaceDestroyed(SurfaceHolder holder) {
+		hasSurface = false;
+
+	}
+
+	public ViewfinderView getViewfinderView() {
+		return viewfinderView;
+	}
+
+	public Handler getHandler() {
+		return handler;
+	}
+
+	public void drawViewfinder() {
+		viewfinderView.drawViewfinder();
+
+	}
+
+	public void handleDecode(Result obj, Bitmap barcode) {
+		inactivityTimer.onActivity();
+//		viewfinderView.drawResultBitmap(barcode);
+		playBeepSoundAndVibrate();
+		resultView.setText(obj.getBarcodeFormat().toString() + ":"
+		        + obj.getText());
+		Intent intent = new Intent(getIntent().getAction());
+		intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
+		intent.putExtra(Intents.Scan.RESULT, obj.toString());
+		intent.putExtra(Intents.Scan.RESULT_FORMAT, obj.getBarcodeFormat()
+		        .toString());
+		sendReplyMessage(R.id.return_scan_result, intent);
+
+	}
+
+	private void sendReplyMessage(int id, Object arg) {
+		Message message = Message.obtain(handler, id, arg);
+		long resultDurationMS = 100;
+		if (resultDurationMS > 0L) {
+			handler.sendMessageDelayed(message, resultDurationMS);
+		} else {
+			handler.sendMessage(message);
+		}
+	}
+
+	/**
+	 * 扫描完成后加载声音
+	 */
+	private void initBeepSound() {
+		if (playBeep && mediaPlayer == null) {
+			// The volume on STREAM_SYSTEM is not adjustable, and users found it
+			// too loud,
+			// so we now play on the music stream.
+			setVolumeControlStream(AudioManager.STREAM_MUSIC);
+			mediaPlayer = new MediaPlayer();
+			mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+			mediaPlayer.setOnCompletionListener(beepListener);
+
+			AssetFileDescriptor file = getResources().openRawResourceFd(
+			        R.raw.beep);
+			try {
+				mediaPlayer.setDataSource(file.getFileDescriptor(),
+				        file.getStartOffset(), file.getLength());
+				file.close();
+				mediaPlayer.setVolume(BEEP_VOLUME, BEEP_VOLUME);
+				mediaPlayer.prepare();
+			} catch (IOException e) {
+				mediaPlayer = null;
+			}
+		}
+	}
+
+	private static final long VIBRATE_DURATION = 200L;
+
+	private void playBeepSoundAndVibrate() {
+		if (playBeep && mediaPlayer != null) {
+			mediaPlayer.start();
+		}
+		if (vibrate) {
+			Vibrator vibrator = (Vibrator) getSystemService(VIBRATOR_SERVICE);
+			vibrator.vibrate(VIBRATE_DURATION);
+		}
+	}
+
+	/**
+	 * When the beep has finished playing, rewind to queue up another one.
+	 */
+	private final OnCompletionListener beepListener = new OnCompletionListener() {
+		public void onCompletion(MediaPlayer mediaPlayer) {
+			mediaPlayer.seekTo(0);
+		}
+	};
+
+
+	// 打开闪光灯
+	private void shanguangdeng() {
+
+		if (isOpenFlash) {
+	        CameraManager.get().closeFlash();
+	        isOpenFlash= false;
+        }else {
+			CameraManager.get().OpenFlash();
+			isOpenFlash = true;
+		}
+
+	}
+
+
+}
---
+++ app/src/main/java/com/barcode/TestCaptureActivity.java
@@ -0,0 +1,172 @@
+package com.barcode;
+
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.PixelFormat;
+import android.hardware.Camera;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.barcode.camera.PlanarYUVLuminanceSource;
+import com.google.zxing.BinaryBitmap;
+import com.google.zxing.MultiFormatReader;
+import com.google.zxing.Result;
+import com.google.zxing.common.HybridBinarizer;
+import com.hebccc.xbp.R;
+
+import java.io.IOException;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class TestCaptureActivity extends Activity {
+	/** Called when the activity is first created. */
+	private SurfaceView sfvCamera;
+	private SFHCamera sfhCamera;
+	private ImageView imgView;
+	private View centerView;
+	private TextView txtScanResult;
+	private Timer mTimer;
+	private MyTimerTask mTimerTask;
+	// 按照标准HVGA
+	final static int width = 480;
+	final static int height = 320;
+	int dstLeft, dstTop, dstWidth, dstHeight;
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.capture_test);
+		imgView = (ImageView) this.findViewById(R.id.ImageView01);
+		centerView = (View) this.findViewById(R.id.centerView);
+		sfvCamera = (SurfaceView) this.findViewById(R.id.sfvCamera);
+		sfhCamera = new SFHCamera(sfvCamera.getHolder(), width, height,
+				previewCallback);
+		txtScanResult=(TextView)this.findViewById(R.id.txtScanResult);
+		// 初始化定时器
+		mTimer = new Timer();
+		mTimerTask = new MyTimerTask();
+		mTimer.schedule(mTimerTask, 0, 80);
+	}
+
+	class MyTimerTask extends TimerTask {
+		@Override
+		public void run() {
+			if (dstLeft == 0) {//只赋值一次
+				dstLeft = centerView.getLeft() * width
+						/ getWindowManager().getDefaultDisplay().getWidth();
+				dstTop = centerView.getTop() * height
+						/ getWindowManager().getDefaultDisplay().getHeight();
+				dstWidth = (centerView.getRight() - centerView.getLeft())* width
+						/ getWindowManager().getDefaultDisplay().getWidth();
+				dstHeight = (centerView.getBottom() - centerView.getTop())* height
+						/ getWindowManager().getDefaultDisplay().getHeight();
+			}
+			sfhCamera.AutoFocusAndPreviewCallback();
+		}
+	}
+	/**
+	 *  自动对焦后输出图片
+	 */
+	private Camera.PreviewCallback previewCallback = new Camera.PreviewCallback() {
+		@Override
+		public void onPreviewFrame(byte[] data, Camera arg1) {
+			//取得指定范围的帧的数据
+			PlanarYUVLuminanceSource source = new PlanarYUVLuminanceSource(
+					data, width, height, dstLeft, dstTop, dstWidth, dstHeight);
+			//取得灰度图
+			Bitmap mBitmap = source.renderCroppedGreyscaleBitmap();
+			//显示灰度图
+			imgView.setImageBitmap(mBitmap);
+			BinaryBitmap bitmap = new BinaryBitmap(new HybridBinarizer(source));
+			MultiFormatReader reader = new MultiFormatReader();
+			try {
+				Result result = reader.decodeWithState(bitmap);
+				String strResult = "BarcodeFormat:"
+						+ result.getBarcodeFormat().toString() + "  text:"
+						+ result.getText();
+				txtScanResult.setText(strResult);
+			} catch (Exception e) {
+				txtScanResult.setText("Scanning");
+			} finally {
+				reader.reset();
+			}
+
+
+
+
+		}
+	};
+}
+
+
+class SFHCamera implements SurfaceHolder.Callback{
+	private SurfaceHolder holder = null;
+	private Camera mCamera;
+	private int width,height;
+	private Camera.PreviewCallback previewCallback;
+
+	public SFHCamera(SurfaceHolder holder,int w,int h,Camera.PreviewCallback previewCallback) {
+		this.holder = holder;  
+		this.holder.addCallback(this);  
+		this.holder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+		width=w;
+		height=h;
+		this.previewCallback=previewCallback;
+	}
+
+	@Override
+	public void surfaceChanged(SurfaceHolder arg0, int arg1, int arg2, int arg3) {
+		Camera.Parameters parameters = mCamera.getParameters();  
+		parameters.setPreviewSize(width, height);//设置尺寸  
+		parameters.setPictureFormat(PixelFormat.JPEG);
+		mCamera.setParameters(parameters);  
+		mCamera.startPreview();//开始预览
+		Log.e("Camera","surfaceChanged");
+	}
+	@Override
+	public void surfaceCreated(SurfaceHolder arg0) {
+		mCamera = Camera.open();//启动服务  
+		try {  
+			mCamera.setPreviewDisplay(holder);//设置预览 
+			Log.e("Camera","surfaceCreated");
+		} catch (IOException e) {  
+			mCamera.release();//释放  
+			mCamera = null;  
+		}
+
+	}
+	@Override
+	public void surfaceDestroyed(SurfaceHolder arg0) {
+		mCamera.setPreviewCallback(null);
+		mCamera.stopPreview();//停止预览  
+		mCamera = null;
+		Log.e("Camera","surfaceDestroyed");
+	}
+	/**
+	 * 自动对焦并回调Camera.PreviewCallback
+	 */
+	public void AutoFocusAndPreviewCallback()
+	{
+		if(mCamera!=null)
+			mCamera.autoFocus(mAutoFocusCallBack);
+	}
+
+	/**
+	 * 自动对焦
+	 */
+	private Camera.AutoFocusCallback mAutoFocusCallBack = new Camera.AutoFocusCallback() {  
+
+		@Override  
+		public void onAutoFocus(boolean success, Camera camera) {      
+			if (success) {  //对焦成功，回调Camera.PreviewCallback
+				mCamera.setOneShotPreviewCallback(previewCallback); 
+			}  
+		}  
+	};
+
+
+}
