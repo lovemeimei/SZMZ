4850ffccf99b81abbf7d4dee7030b202fa18bac9
xuan <386638790@qq.com>
2018-07-25 17:25:10 +0800

    0725

--- app/src/main/java/com/szmz/SystemConst.java
+++ app/src/main/java/com/szmz/SystemConst.java
@@ -5,14 +5,6 @@ package com.szmz;
  */
 public class SystemConst {
 
-//    //新疆
-//    public static final String DEFAULT_SERVER_COM = "http://222.82.228.126:7780/";//平台
-//    public static final String DEFAULT_SERVER_COM_SQR = "http://222.82.228.126:80/";//新疆平台
-//    public static final String IP_JZ="222.82.228.126:3001";
-//    public static final String IP_SM="222.82.231.35:3003";
-//    public static final String IP_YZS="222.82.231.35:3003";
-//    public static final String IP_HD="222.82.228.126:3002";
-
 
     public static final String DEFAULT_SERVER_COM = "http://222.222.49.34:8057/";//平台
     public static final String DEFAULT_SERVER_COM_SQR = "http://222.222.49.34:8081/";//平台
@@ -27,12 +19,6 @@ public class SystemConst {
     public static final String DEFAULT_SERVER = "http://"+IP_HD+"/";//核对
     public static final String PROJECT_DIR = "SZMZ";
     public static final int MaxPhotoNumber = 9;
-
-
-    public static  String SystemID_JZ="";
-    public static  String SystemID_YZS="";
-    public static  String SystemID_SH="";
-    public static  String SystemID_SM="";
 
     //1是新疆2是攀枝花
     public static  int systemID=2;
